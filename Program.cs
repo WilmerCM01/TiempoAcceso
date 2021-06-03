c7631d86c6171ff315c45ccabc6c226aad59f8ff
WilmerCM01 <84152101+WilmerCM01@users.noreply.github.com>
2021-06-02 21:33:37 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Programas
+{
+    class Program
+    {
+		
+        static void Main(string[] args){
+
+			int[] prueba = new int[1000000000];
+			int contador = 0;
+			
+			Stopwatch crono = new Stopwatch();
+			
+			crono.Start();
+			
+			for(int i = 999999999; i >= 0; i--){
+				
+				if(i == 0){
+					
+					prueba[i] = 15;
+					crono.Stop();
+					Console.WriteLine("Tiempo: " + crono.Elapsed);
+					
+				}
+				
+				contador+=1;
+				
+			}
+			
+			Console.WriteLine("Cantidad de iteraciones: " + contador);
+			
+			contador = 0;
+			crono.Restart();
+			crono.Start();
+			
+			for(int a = 0; a < prueba.Length; a++){
+				
+				if(a == 999999999){
+					
+					prueba[a] = 15;
+					crono.Stop();
+					Console.WriteLine("Tiempo: " + crono.Elapsed);
+					
+				}
+				
+				contador+=1;
+				
+			}
+			
+			Console.WriteLine("Cantidad de iteraciones: " + contador);
+
+            Console.WriteLine();
+            Console.WriteLine("Presione cualquier tecla para finalizar...");
+            Console.ReadKey();
+
+        }
+
+    }
+}
